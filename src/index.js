0e57e44391be147bfcda1786d617ea1416956780
Sandip Swain <swainsandip9@gmail.com>
2021-05-19 11:41:03 +0530

    Setup for WebSocket.io

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const port = process.env.PORT || 3000;
+
+// Define path for express config
+const publicDirpath = path.join(__dirname, "../public");
+
+// Setup static directory to serve
+app.use(express.static(publicDirpath));
+
+// Port access
+server.listen(port, () => {
+  console.log(`Server is up on PORT ${port}`);
+});
