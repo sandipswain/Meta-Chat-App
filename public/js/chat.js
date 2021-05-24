787bebd790a7b1703c5c56fcc7a5cf5df22edc4a
Sandip Swain <swainsandip9@gmail.com>
2021-05-24 12:13:44 +0530

    Enabled and Disabled the input form and the location

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,22 +1,36 @@
 const socket = io();
-
-const submit = document.querySelector("#message-form");
-const sendLocation = document.querySelector("#send-location");
+// Elements
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $sendLocation = document.querySelector("#send-location");
 // Receive the event the server sent to client
 socket.on("message", (message) => console.log(message));
 
-submit.addEventListener("submit", (e) => {
+$messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
+  // disable the form once submitted
+  $messageFormButton.setAttribute("disabled", "disabled");
+
   const message = e.target.elements.message.value;
   socket.emit("sendMessage", message, (error) => {
+    // Enable the form
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+    // Move the focus back to the input field
+
     if (error) return console.log(error);
     console.log("Message delivered!");
   });
 });
 
-sendLocation.addEventListener("click", () => {
+$sendLocation.addEventListener("click", () => {
   if (!navigator.geolocation)
     return alert("Geolocation is not supported by your browser");
+
+  // Disable the send location after sent
+  $sendLocation.setAttribute("disabled", "disabled");
 
   navigator.geolocation.getCurrentPosition((position, callback) => {
     // console.log(position);
@@ -27,6 +41,8 @@ sendLocation.addEventListener("click", () => {
         longitude: position.coords.longitude,
       },
       () => {
+        // Enable sendLocation
+        $sendLocation.removeAttribute("disabled");
         console.log("Location shared!");
       }
     );
