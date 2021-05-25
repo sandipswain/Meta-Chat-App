8ccc9e3633fe28a9d90863564ecfd2a006233012
Sandip Swain <swainsandip9@gmail.com>
2021-05-25 18:07:05 +0530

    Added timestamp to messages

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
 const Filter = require("bad-words");
+const { generateMessage } = require("./utils/messages");
 
 const app = express();
 const server = http.createServer(app);
@@ -25,9 +26,9 @@ let count = 0;
 io.on("connection", (socket) => {
   console.log("New Websocket Connection");
 
-  socket.emit("message", "Welcome");
+  socket.emit("message", generateMessage("Welcome!"));
   // Broadcast lets everyone know that the user has joined except the user that has joined
-  socket.broadcast.emit("message", "A new user has joined");
+  socket.broadcast.emit("message", generateMessage("A new user has joined!"));
 
   // Listening an event from a client
   socket.on("sendMessage", (message, callback) => {
@@ -36,14 +37,14 @@ io.on("connection", (socket) => {
       return callback("Profanity is not allowed");
     }
     // This emits the event to evry connected client
-    io.emit("message", message);
+    io.emit("message", generateMessage(message));
     callback("Delivered");
   });
 
   // To notify all Users that a user has shared its location
   socket.on("sendLocation", (coords, callback) => {
     io.emit(
-      "message",
+      "locationMessage",
       `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
     );
     callback();
@@ -51,7 +52,7 @@ io.on("connection", (socket) => {
 
   // To notify all the clients connected to the server when the user gets disconnected
   socket.on("disconnect", () => {
-    io.emit("message", "A user has left the server");
+    io.emit("message", generateMessage("A user has left the server"));
   });
 });
 
